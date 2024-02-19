87064c7f1651c515200eab3c2f83bc78ec11a0d3
LaraZwei <nilsv-b@gmx.de>
2024-02-20 00:05:35 +0100

    waiting, rebuild etc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,8 +65,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "2530e9d99150c9c81ca16ecc5318361a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "35c18d347a7884c7c196906bf6070543",
-"/": "35c18d347a7884c7c196906bf6070543",
+"index.html": "fc3ee7d82dea6bf8277e654af879d3fd",
+"/": "fc3ee7d82dea6bf8277e654af879d3fd",
 "main.dart.js": "47fd17813d21eec91f78cb301123df53",
 "manifest.json": "19913adab3f1b95d4aa86584651108f7",
 "version.json": "5bbb4289c32d4ac505c373541db49109"};
